e7503a85b960f8b14228b08094fd287ccc0e2beb
malk-carlos <81245539+malk-carlos@users.noreply.github.com>
2021-11-08 15:21:44 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+function kamigolist(){
+    var kaminogo = "柿食えば,ラララライ,「寒いね」と,この味がいいねと,邪魔するな,角川が";
+    kaminogo = kaminogo.split(",");
+
+    return kaminogo;
+}
+
+function shichilist(){
+    var shichi = "鐘が鳴るなり,ララライララライ,話しかければ,君が言ったから,今の私は,ニコニコ動画と,みんなで渡れば";
+    shichi = shichi.split(",");
+
+    return shichi;
+}
+
+function shimogolist(){
+    var shimonogo = "法隆寺,ラララライ,「寒いね」と,,忙しい,手を組んだ,怖くない";
+    shimonogo = shimonogo.split(",");
+
+    return shimonogo;
+}
---
+++ index.js
@@ -0,0 +1,63 @@
+function sentan(){
+    // 5を選択（ランダム）
+    var kamigo = kamigolist();
+    var kamigo_len = kamigo.length;
+    var ransuuA = Math.floor(Math.random() * kamigo_len);
+    document.getElementById("a").innerHTML = kamigo[ransuuA];
+
+    // 7を選択（ランダム）
+    var shichi = shichilist();
+    var shichi_len = shichi.length;
+    var ransuuB = Math.floor(Math.random() * shichi_len);
+    document.getElementById("b").innerHTML = shichi[ransuuB];
+
+    // 5を選択（ランダム）
+    var shimogo = shimogolist();
+    var shimogo_len = shimogo.length;
+    var ransuuC = Math.floor(Math.random() * shimogo_len);
+    document.getElementById("c").innerHTML = shimogo[ransuuC];
+    // ランダム部分をデータ配列から選ばれるように（単語としての体をなすため）
+
+    // ランダムに下の句を変更する
+    var ransuuS1 = Math.floor(Math.random() * 2);
+    if(ransuuS1 === 0){
+        $("#salad").css("display","none");
+        $("#akohusi").css("display","unset");
+    } else if(ransuuS1 === 1){
+        $("#akohusi").css("display","none");
+        $("#salad").css("display","unset");
+    }
+}
+
+function simo(){
+    // 下の句 を表示/非表示（選択を切り替えた時）
+    var sel = document.getElementById("select").value;
+
+    if( sel === "tan" ){
+        $("#simo").css("display","unset");
+        // $("#akohusi").css("display","none");
+        var ransuuS2 = Math.floor(Math.random() * 2);
+        if(ransuuS2 === 0){
+            $("#salad").css("display","none");
+            $("#akohusi").css("display","unset");
+        } else if(ransuuS2 === 1){
+            $("#akohusi").css("display","none");
+            $("#salad").css("display","unset");
+        }
+    } else if ( sel === "sen" ){
+        $("#simo").css("display","none");
+    }
+}
+
+
+// function up(){
+//     var file = filelist(); //data.jsの関数を呼び出す
+//     var fileno = file.length;
+//     var sosu = ''; //ここで初期化しておかないと連結時に'undefined'が先頭に表示されてしまう
+//     for(i=0;i<fileno;i++){
+//     //var sosu = sosu +'<label><input type=radio name=mus onClick="func1(this)" onkeypress="func1(this)" value="'+ file[i] +'">'+ file[i] +'</label><br>';
+//     //html要素の属性はダブルコーテーションでくくる、終了タグのスラッシュはエスケープしておく
+//     sosu = sosu +'<label><input type="radio" name="mus" onClick="func1(this)" onkeypress="func1(this)" value="'+ file[i] +'">'+ file[i] +'<\/label><br>';
+//     }
+//     document.getElementById('hyouji').innerHTML = sosu;
+//     }
